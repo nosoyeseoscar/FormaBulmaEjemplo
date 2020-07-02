c9a423a234a25211a5d0b3c3c90cbfb99edf1276
Oscar Fuentes <ofuentese@gmail.com>
2020-07-02 12:30:37 -0600

    primera versión

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -20,43 +20,49 @@ function Login() {
     console.log(data);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="control has-icons-left">
-        <input
-          className="input is-primary is-medium"
-          name="email"
-          type="text"
-          placeholder="Correo electrónico"
-          ref={register({
-            required: "Correo electrónico obligatorio",
-            minLength: { value: 3, message: "correo demasiado corto" },
-          })}
-        />
-        <span className="icon is-small is-left mt-3">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </span>
+    <div className="card">
+      <div className="card-content">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="control has-icons-left">
+            <input
+              className="input is-link is-medium my-3"
+              name="email"
+              type="text"
+              placeholder="Correo electrónico"
+              ref={register({
+                required: "Correo electrónico obligatorio",
+                minLength: { value: 3, message: "correo demasiado corto" },
+              })}
+            />
+            <span className="icon is-small is-left mt-3">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+          </div>
+          {errors.email && (
+            <p className="help is-danger">{errors.email.message}</p>
+          )}
+          <div className="control has-icons-left">
+            <input
+              className="input is-link is-medium my-3"
+              name="password"
+              type="password"
+              placeholder="Contraseña"
+              ref={register({
+                required: "Contraseña obligatoria",
+                minLength: { value: 3, message: "contraseña demasiado corta" },
+              })}
+            />
+            <span className="icon is-small is-left mt-3">
+              <FontAwesomeIcon icon={faKey} />
+            </span>
+          </div>
+          {errors.password && (
+            <p className="help is-danger">{errors.password.message}</p>
+          )}
+          <input type="submit" className="button is-link is-medium my-3 is-fullwidth" />
+        </form>
       </div>
-      {errors.email && <p className="help is-danger">{errors.email.message}</p>}
-      <div className="control has-icons-left">
-        <input
-          className="input is-primary is-medium"
-          name="password"
-          type="password"
-          placeholder="Contraseña"
-          ref={register({
-            required: "Contraseña obligatoria",
-            minLength: { value: 3, message: "contraseña demasiado corta" },
-          })}
-        />
-        <span className="icon is-small is-left mt-3">
-          <FontAwesomeIcon icon={faKey} />
-        </span>
-      </div>
-      {errors.password && (
-        <p className="help is-danger">{errors.password.message}</p>
-      )}
-      <input type="submit" className="button is-primary is-medium" />
-    </form>
+    </div>
   );
 }
 
